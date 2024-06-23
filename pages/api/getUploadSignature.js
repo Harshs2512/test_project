80d144e6ac5d78e163be456b5249f39112a4d1ca
Harshs2512 <harshsoni1234ji@gmail.com>
2024-06-23 18:22:35 +0530

    harsh

--- pages/api/getUploadSignature.js
+++ pages/api/getUploadSignature.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     res.status(200).json({
         signature: signature,
         timestamp: timestamp,
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY
     });
 }
